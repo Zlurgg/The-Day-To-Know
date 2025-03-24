ca0c72a3e1f9aa286984c3cded046527a3d5adc9
Zlurgg <joseph.brightman@gmail.com>
2025-03-24 13:14:55 +0000

    Model for article

---
+++ app/src/main/java/uk/co/zlurgg/thedaytoknow/article/domain/Article.kt
@@ -0,0 +1,13 @@
+package uk.co.zlurgg.thedaytoknow.article.domain
+
+data class Article(
+    val id: String,
+    val title: String,
+    val category: String,
+    val summary: String,
+    val country: String,
+    val language: String,
+    val publisher: String,
+    val publishedDate: String,
+    val linkToArticle: String
+)
