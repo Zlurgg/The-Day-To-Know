03cd0065ebbfd175c5f5bd576257dc0a036586b1
Zlurgg <joseph.brightman@gmail.com>
2025-03-25 15:15:31 +0000

    search bar

--- app/src/main/java/uk/co/zlurgg/thedaytoknow/news/presentation/news_reel/components/ArticleSearchBar.kt
+++ app/src/main/java/uk/co/zlurgg/thedaytoknow/news/presentation/news_reel/components/ArticleSearchBar.kt
@@ -1,14 +1,26 @@
 package uk.co.zlurgg.thedaytoknow.news.presentation.news_reel.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import uk.co.zlurgg.thedaytoknow.R
 import uk.co.zlurgg.thedaytoknow.core.presentation.ui.theme.TheDayToKnowTheme
@@ -25,14 +37,54 @@ fun ArticleSearchBar(
         onValueChange = onSearchQueryChange,
         shape = RoundedCornerShape(100),
         colors = OutlinedTextFieldDefaults.colors(
-            cursorColor = Color.DarkGray,
-            focusedBorderColor = Color.LightGray,
+            cursorColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f),
+            focusedBorderColor = MaterialTheme.colorScheme.onPrimaryContainer,
         ),
         placeholder = {
             Text(
                 text = stringResource(id = R.string.search_hint)
             )
-        }
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
+            )
+        },
+        singleLine = true,
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onImeSearch()
+            }
+        ),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Search
+        ),
+        trailingIcon = {
+            AnimatedVisibility(
+                visible = searchQuery.isNotBlank()
+            ) {
+                IconButton(
+                    onClick = {
+                        onSearchQueryChange("")
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = stringResource(R.string.close_hint),
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            }
+        },
+        modifier = modifier
+            .background(
+                shape = RoundedCornerShape(100),
+                color = MaterialTheme.colorScheme.background
+            )
+            .minimumInteractiveComponentSize()
     )
 }
 
@@ -41,7 +93,7 @@ fun ArticleSearchBar(
 private fun ArticleSearchBarPreview() {
      TheDayToKnowTheme {
          ArticleSearchBar(
-             searchQuery = "Latest news",
+             searchQuery = "",
              onSearchQueryChange = {},
              onImeSearch = {},
              modifier = Modifier
